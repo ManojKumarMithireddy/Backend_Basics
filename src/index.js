2a9d816be3e32fbdd30e932080227028b0e2908f
MANOJ KUMAR MITHIREDDY <148680918+Manoj-kumar2305@users.noreply.github.com>
2024-08-25 06:22:43 +0000

    intial utils configuration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import dotenv from 'dotenv';
-import connectDB from './db/index.js';
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import {app} from "./app.js";
 
-dotenv.config({path: '.env'});
+dotenv.config({ path: ".env" });
 
-connectDB();
+connectDB().then(()=>{
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+}).catch((err) => console.error(err));
