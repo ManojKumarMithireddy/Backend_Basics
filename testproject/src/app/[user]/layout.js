a1864de5398a24c8a6243121f7509e14cceb30d8
MANOJ KUMAR MITHIREDDY <148680918+ManojKumarMithireddy@users.noreply.github.com>
2024-12-27 10:02:14 +0000

    usual commit

--- testproject/src/app/[user]/layout.js
+++ testproject/src/app/[user]/layout.js
@@ -1,6 +1,6 @@
 // components/Layout.js
 'use client';
-import React, {useEffect} from 'react';
+import React, { Suspense, useEffect } from 'react';
 import { VscAccount } from 'react-icons/vsc';
 import { BiHomeAlt } from 'react-icons/bi';
 import { MdOutlineSettings } from 'react-icons/md';
@@ -8,60 +8,67 @@ import './layout.css';
 import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
 import { useUserStore } from '@/store/userStore';
-
+import Loading from '@/app/loading';
 
 const Layout = ({ children }) => {
-    const pathname = usePathname();
-    const router = useRouter();
-    const user = useUserStore((state) => state.user);
+  const pathname = usePathname();
+  const router = useRouter();
+  const user = useUserStore((state) => state.user);
 
-  
-    useEffect(() => {
-      // if user is not logged in, redirect to login page
-      if (!user.userName) {
-        alert('Please login to continue');
-        router.push('/');
-      }
-    }, [user.userName]);
-  
-    const handleLogout = () => {
-      // clear the local storage and redirect to login page
-      if (typeof window !== 'undefined') {
-        localStorage.removeItem('user');
-      }
-    };
+  useEffect(() => {
+    // if user is not logged in, redirect to login page
+    if (!user.userName) {
+      alert('Please login to continue');
+      router.push('/');
+    }
+  }, [user.userName]);
+
+  const handleLogout = () => {
+    // clear the local storage and redirect to login page
+    if (typeof window !== 'undefined') {
+      localStorage.removeItem('user');
+    }
+  };
   return (
     <>
       <header>
         <h1>Dashboard</h1>
         <div className="user-info">
-          <span><Link href="/" className='logout' onClick={handleLogout}>Logout</Link></span>
+          <span>
+            <Link href="/" className="logout" onClick={handleLogout}>
+              Logout
+            </Link>
+          </span>
         </div>
       </header>
       <div className="main-content">
         <div className="sidebar">
           <ul>
             <li>
-              <Link href={`/${user.userName}`} className={pathname === `/${user.userName}` ? 'active' : ''}>
-                <BiHomeAlt className='icon'/>
+              <Link
+                href={`/${user.userName}`}
+                className={pathname === `/${user.userName}` ? 'active' : ''}>
+                <BiHomeAlt className="icon" />
                 <span>Home</span>
               </Link>
             </li>
             <li>
               <Link href="#">
-                <VscAccount className='icon'/>
+                <VscAccount className="icon" />
                 <span>Users</span>
               </Link>
             </li>
             <li>
               <Link href="#">
-                <MdOutlineSettings className='icon'/>
+                <MdOutlineSettings className="icon" />
                 <span>Settings</span>
               </Link>
             </li>
           </ul>
         </div>
-        <div className="cards">{children}</div>
+        <Suspense fallback={<Loading />}>
+          <div className="cards">{children}</div>
+        </Suspense>
       </div>
     </>
   );
